45d14b0374cd77fe981c9326d48f6366b5b7df5b
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-05-23 09:26:45 +0200

    Add files via upload

---
+++ 23.05. 2022/23.05. 2022/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace _23._05._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.odpoved = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.odpoved2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Obsahuje číslo?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Odpověd:";
+            // 
+            // odpoved
+            // 
+            this.odpoved.AutoSize = true;
+            this.odpoved.Location = new System.Drawing.Point(146, 137);
+            this.odpoved.Name = "odpoved";
+            this.odpoved.Size = new System.Drawing.Size(0, 13);
+            this.odpoved.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Obsahuje slovo?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Odpověd:";
+            // 
+            // odpoved2
+            // 
+            this.odpoved2.AutoSize = true;
+            this.odpoved2.Location = new System.Drawing.Point(376, 78);
+            this.odpoved2.Name = "odpoved2";
+            this.odpoved2.Size = new System.Drawing.Size(0, 13);
+            this.odpoved2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.odpoved2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.odpoved);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label odpoved;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label odpoved2;
+    }
+}
+
---
+++ 23.05. 2022/23.05. 2022/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23._05._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool JeCislo(string vstup, ref int pocetCifer, ref int soucet)
+        {
+            pocetCifer = 0;
+            soucet = 0;
+            for(int i = 0;i < vstup.Length ;i++)
+            {
+                if(vstup[i] >= '0'&& vstup[i] <='9')
+                {
+                    pocetCifer++;
+                    if(vstup[i] % 2 !=0)
+                    {
+                        soucet += (vstup[i] - 48);
+                    }
+                }    
+            }
+            if (pocetCifer > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool ObsahujeSlovo(string vstup, out string Nejdelsi, out string Nejkratsi)
+        {
+            Nejdelsi = "";
+            string[] slova = vstup.Split();
+            Nejkratsi = slova[0];
+            while(vstup.Contains("  "))
+            {
+                vstup = vstup.Replace("  ", " ");
+            }
+            foreach(string s in slova)
+            {
+                if(s.Length > Nejdelsi.Length)
+                {
+                    Nejdelsi = s;
+                }
+                if(s.Length < Nejkratsi.Length)
+                {
+                    Nejkratsi = s;
+                }
+            } 
+            if(slova[0] == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocetCifer = 0, soucet = 0;
+            odpoved.Text = String.Format("Text {0}obsahuje číslo.\nPočet čísel: {1}\nSoučet lychcých čísel: {2}", JeCislo(textBox1.Text, ref pocetCifer, ref soucet) ? "" : "ne",pocetCifer,soucet);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Nejdelsi, Nejkratsi;
+            odpoved2.Text = String.Format("Text {0}obsahuje slovo.\nNejdelší slovo: {1}\nNejkratší slovo: {2}",ObsahujeSlovo(textBox1.Text, out Nejdelsi, out Nejkratsi) ? "" : "ne", Nejdelsi, Nejkratsi);
+        }
+    }
+}
